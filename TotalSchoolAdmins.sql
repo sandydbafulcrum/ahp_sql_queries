e12181cb04abe5e73411139a9ece161dbf799a7a
sandydbafulcrum <99655260+sandydbafulcrum@users.noreply.github.com>
2022-03-07 19:15:14 +0800

    update to get only total school admin
    Update to just count the total school admin that used the application on  a given date

--- TotalSchoolAdmins.sql
+++ TotalSchoolAdmins.sql
@@ -1,8 +1,6 @@
- SELECT FORMAT(Timestamp,'yyyy-mm-dd') AS PerDay,
+ SELECT 
         COUNT(*) AS TotalSchoolAdmins
  FROM   AuditEvents
  WHERE [Timestamp] BETWEEN '2022-02-18' AND '2022-03-02'
  AND EventName = 'Token Issued Success'
- AND ClientID = 'school_admin_experience'
- GROUP BY FORMAT(Timestamp,'yyyy-mm-dd')
- ORDER BY PerDay;
+ AND ClientID = 'school_admin_experience';
