47151c9d380b1e2ff579c5d4cfb5f50a566076d4
sandydbafulcrum <99655260+sandydbafulcrum@users.noreply.github.com>
2022-03-07 19:13:28 +0800

    update to just count total student user
    Update to just count total count of user on a given date range.

--- TotalStudents.sql
+++ TotalStudents.sql
@@ -1,8 +1,6 @@
- SELECT FORMAT(Timestamp,'yyyy-mm-dd') AS PerDay,
+ SELECT 
         COUNT(*) AS TotalStudent
  FROM   AuditEvents
  WHERE [Timestamp] BETWEEN '2022-02-18' AND '2022-03-02'
  AND EventName = 'Token Issued Success'
- AND ClientID = 'student_experience'
- GROUP BY FORMAT(Timestamp,'yyyy-mm-dd')
- ORDER BY PerDay;
+ AND ClientID = 'student_experience';
