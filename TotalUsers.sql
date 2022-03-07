1a36934d38c550881c4a71674934d8e9833f4e88
sandydbafulcrum <99655260+sandydbafulcrum@users.noreply.github.com>
2022-03-07 19:11:35 +0800

    Update to just count total of users
    Count total count of users on a certain date range. Reminded to just copy what datadog captures.

--- TotalUsers.sql
+++ TotalUsers.sql
@@ -1,7 +1,5 @@
- SELECT FORMAT(Timestamp,'yyyy-mm-dd') AS PerDay,
+ SELECT 
         COUNT(*) AS TotalUsers
  FROM   AuditEvents
  WHERE [Timestamp] BETWEEN '2022-02-18' AND '2022-03-02'
- AND EventName = 'Token Issued Success'
- GROUP BY FORMAT(Timestamp,'yyyy-mm-dd')
- ORDER BY PerDay;
+ AND EventName = 'Token Issued Success';
