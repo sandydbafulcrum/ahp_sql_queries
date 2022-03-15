f2a898e8065505595a73a9dd20c7e8e15076f66d
sandydbafulcrum <99655260+sandydbafulcrum@users.noreply.github.com>
2022-03-16 03:52:40 +0800

    Create SuccessLoginCountry.sql
    Get report users IP to find country later

---
+++ SuccessLoginCountry.sql
@@ -0,0 +1,9 @@
+ SELECT 
+    COUNT(Id),
+    RemoteIpAddress,
+    FORMAT(Timestamp,'yyyy-mm-dd') AS PerDay
+ FROM   AuditEvents
+ WHERE [Timestamp] BETWEEN '2022-02-18' AND '2022-03-02'
+ AND EventName IN ('Token Issued Success', 'User Login Success')
+ GROUP BY FORMAT(Timestamp,'yyyy-mm-dd'), RemoteIpAddress
+ ORDER BY PerDay;
