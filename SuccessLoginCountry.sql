8fdb89859caf1ced4de4da9cb4c5be74be236cde
sandydbafulcrum <99655260+sandydbafulcrum@users.noreply.github.com>
2022-03-17 02:28:44 +0800

    Update SuccessLoginCountry.sql
    Add view creation script and removing order by clause as this is not allowed in view table.

--- SuccessLoginCountry.sql
+++ SuccessLoginCountry.sql
@@ -1,3 +1,4 @@
+CREATE OR ALTER VIEW SuccessLoginCountry AS
  SELECT
     COUNT(Id) as Counts,
     RemoteIpAddress,
@@ -5,5 +6,4 @@
  FROM   AuditEvents
  WHERE 
  EventName IN ('Token Issued Success', 'User Login Success')
- GROUP BY CAST(Timestamp as Date), RemoteIpAddress
- ORDER BY PerDay;
+ GROUP BY CAST(Timestamp as Date), RemoteIpAddress;
