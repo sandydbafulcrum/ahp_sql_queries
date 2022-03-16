bd71241ddd52ac537d7ff33c9e436e294973514c
sandydbafulcrum <99655260+sandydbafulcrum@users.noreply.github.com>
2022-03-17 00:51:19 +0800

    Update TotalSchoolAdmins.sql
    Remove date range

--- TotalSchoolAdmins.sql
+++ TotalSchoolAdmins.sql
@@ -1,6 +1,5 @@
  SELECT 
         COUNT(*) AS TotalSchoolAdmins
  FROM   AuditEvents
- WHERE [Timestamp] BETWEEN '2022-02-18' AND '2022-03-02'
- AND EventName = 'Token Issued Success'
+ WHERE EventName = 'Token Issued Success'
  AND ClientID = 'school_admin_experience';
