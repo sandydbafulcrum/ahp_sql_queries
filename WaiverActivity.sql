c3415534f022dc72ea76be460635680188c31539
sandydbafulcrum <99655260+sandydbafulcrum@users.noreply.github.com>
2022-03-17 02:37:42 +0800

    Update WaiverActivity.sql
    Add view creation script and removing order by clause as this is not allowed in view table.

--- WaiverActivity.sql
+++ WaiverActivity.sql
@@ -1,7 +1,8 @@
+CREATE OR ALTER VIEW WaiverActivity AS
    SELECT 
     COUNT(CASE WHEN RequestPath like '%Submission%' AND RequestMethod = 'PUT' OR RequestMethod = 'POST'  THEN 1 ELSE NULL END) as "Submission",
     COUNT(CASE WHEN RequestPath like '%Waiver' AND RequestMethod = 'POST' THEN 1 ELSE NULL END) as "Waiver",
     CAST(Timestamp as Date) AS PerDay
  FROM   AuditEvents
- GROUP BY CAST(Timestamp as Date)
- ORDER BY PerDay;
+ GROUP BY CAST(Timestamp as Date);
+
