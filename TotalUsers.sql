49b2d9db589e3fda3f94c78f090a92208be4d62c
sandydbafulcrum <99655260+sandydbafulcrum@users.noreply.github.com>
2022-03-17 00:53:21 +0800

    Update TotalUsers.sql
    Remove date range

--- TotalUsers.sql
+++ TotalUsers.sql
@@ -1,5 +1,4 @@
  SELECT 
         COUNT(*) AS TotalUsers
  FROM   AuditEvents
- WHERE [Timestamp] BETWEEN '2022-02-18' AND '2022-03-02'
- AND EventName = 'Token Issued Success';
+ WHERE EventName = 'Token Issued Success';
