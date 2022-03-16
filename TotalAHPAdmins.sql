006bffdeab6a93bb9e35310a9af5ebb2653ecc06
sandydbafulcrum <99655260+sandydbafulcrum@users.noreply.github.com>
2022-03-17 00:48:58 +0800

    Update TotalAHPAdmins.sql
    Remove timestamp date range.

--- TotalAHPAdmins.sql
+++ TotalAHPAdmins.sql
@@ -1,7 +1,5 @@
- SELECT 
+  SELECT 
         COUNT(*) AS TotalAdmins
  FROM   AuditEvents
- WHERE [Timestamp] BETWEEN '2022-02-18' AND '2022-03-02'
- AND EventName = 'Token Issued Success'
+ WHERE EventName = 'Token Issued Success'
  AND ClientID NOT IN ('school_admin_experience', 'student_experience');
-
