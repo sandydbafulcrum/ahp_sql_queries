8ed88f0160172d837e7a0eff074157e1a4931af9
sandydbafulcrum <99655260+sandydbafulcrum@users.noreply.github.com>
2022-03-17 00:49:36 +0800

    Update TotalAuthentications.sql
    Remove timestamp date range

--- TotalAuthentications.sql
+++ TotalAuthentications.sql
@@ -1,5 +1,4 @@
 SELECT 
         COUNT(*) AS TotalAuth
  FROM   AuditEvents
- WHERE [Timestamp] BETWEEN '2022-02-18' AND '2022-03-02'
- AND Category = 'Authentication'
+ WHERE Category = 'Authentication'
