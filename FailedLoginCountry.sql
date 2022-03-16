9d1ebe24aa298a10857e22f72ac7a38f565e83df
sandydbafulcrum <99655260+sandydbafulcrum@users.noreply.github.com>
2022-03-17 00:37:19 +0800

    Update FailedLoginCountry.sql
    Update to fix date formatting error

--- FailedLoginCountry.sql
+++ FailedLoginCountry.sql
@@ -1,9 +1,8 @@
- SELECT 
-    COUNT(Id),
+ SELECT
+    COUNT(Id) as FailedLogin,
     RemoteIpAddress,
-    FORMAT(Timestamp,'yyyy-mm-dd') AS PerDay
+    CAST(Timestamp as Date) as PerDay
  FROM   AuditEvents
- WHERE [Timestamp] BETWEEN '2022-02-18' AND '2022-03-02'
- AND EventName IN ('Token Issued Failure', 'User Login Failure')
- GROUP BY FORMAT(Timestamp,'yyyy-mm-dd'), RemoteIpAddress
- ORDER BY PerDay;
+ WHERE EventName IN ('Token Issued Failure', 'User Login Failure')
+ GROUP BY CAST(Timestamp as Date), RemoteIpAddress
+ ORDER BY perday asc;
