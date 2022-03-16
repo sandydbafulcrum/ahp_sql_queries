4589a577820d25acca9f87a2a3bd7e8dfe1f7461
sandydbafulcrum <99655260+sandydbafulcrum@users.noreply.github.com>
2022-03-17 00:44:13 +0800

    Update StudentActivations.sql
    Fix Date formatting error and remove date range in where clause.

--- StudentActivations.sql
+++ StudentActivations.sql
@@ -1,7 +1,7 @@
- SELECT FORMAT(Timestamp,'yyyy-mm-dd') AS PerDay,
+   SELECT
+   CAST(Timestamp as Date) AS PerDay,
         COUNT(*) AS StudentActivations
  FROM   AuditEvents
- WHERE [Timestamp] BETWEEN '2022-02-18' AND '2022-03-02'
- AND RequestPath = '/api/Users/ActivateAccount'
- GROUP BY FORMAT(Timestamp,'yyyy-mm-dd')
+ WHERE RequestPath = '/api/Users/ActivateAccount'
+ GROUP BY CAST(Timestamp as Date)
  ORDER BY PerDay;
