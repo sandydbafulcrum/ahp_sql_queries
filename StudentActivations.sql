4f25cea889ee457f71cf850ddc3a8da78a88efa8
sandydbafulcrum <99655260+sandydbafulcrum@users.noreply.github.com>
2022-03-17 02:27:39 +0800

    Update StudentActivations.sql
    Add view creation script and removing order by clause as this is not allowed in view table.

--- StudentActivations.sql
+++ StudentActivations.sql
@@ -1,7 +1,7 @@
-   SELECT
+CREATE OR ALTER VIEW StudentActivations AS
+  SELECT
    CAST(Timestamp as Date) AS PerDay,
         COUNT(*) AS StudentActivations
  FROM   AuditEvents
  WHERE RequestPath = '/api/Users/ActivateAccount'
- GROUP BY CAST(Timestamp as Date)
- ORDER BY PerDay;
+ GROUP BY CAST(Timestamp as Date);
